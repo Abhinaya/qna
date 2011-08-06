ff989e47b15e93ae35bdaaaf6f9fc14714b5c390
Senthil/Abhi <senthilv@thoughtworks.com>
2011-08-06 13:13:48 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qna::Application.config.secret_token = '318d60e4f0066378014ea3fb35d2041eefb0ba541d926f2477aa6b0c66ba5b19430437aacb769031c50d38bba405a938c3e2c448af70c707e7107fbb4c5dbf38'
